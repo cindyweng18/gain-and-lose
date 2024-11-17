9109f533870e28122f0625cbacc3aed1f69a836e
cindyweng18 <cindyweng18@gmail.com>
2024-11-17 10:41:30 -0500

    install chakra ui

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { Provider } from "@/components/ui/provider"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,9 +20,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Provider>
+          {children}
+        </Provider>
       </body>
     </html>
   );
