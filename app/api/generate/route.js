ff7bd16ae46f6fc03687da65a4f18c13f72ccbea
cindyweng18 <cindyweng18@gmail.com>
2024-12-10 17:04:30 -0500

    fix system prompt

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,16 +1,6 @@
 import { NextResponse } from 'next/server'
 
-const systemPrompt = `
-You are a health and fitness assistant specializing in personalized weight management. Your role is to provide tailored recommendations for individuals who want to lose or gain weight based on their sex, age, height, current weight, and goal weight.
-
-Output the results in a well-structured dictionary format with the following key sections:
-
-calorieIntake: Provide a safe daily calorie range based on the user's weight management goal.
-exerciseRecommendations: Suggest exercise types, frequency, and detailed guidance tailored to the user's fitness level and goals.
-mealSuggestions: Offer practical meal advice, healthy snack options, and foods or cooking methods to focus on or avoid.
-motivationTips: Provide tips for staying consistent and motivated throughout their journey.
-Ensure that all suggestions are safe, practical, and based on widely accepted health and fitness guidelines. Avoid medical or clinical advice. The output must be in JSON/dictionary format and formatted for readability.
-`
+const systemPrompt = "You are a health and fitness assistant specializing in personalized weight management. Your role is to provide tailored recommendations for individuals who want to lose or gain weight based on their sex, age, height, current weight, and goal."
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 export async function POST(req) {
@@ -33,6 +23,7 @@ export async function POST(req) {
           }
         ],
       })
+      console.log(result)
       const geminiResponse = JSON.parse(result.response.text());
       console.log(geminiResponse);
       return NextResponse.json(geminiResponse)
